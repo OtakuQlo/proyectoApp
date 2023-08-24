0ccb0bcba5cfd33b4aaa4db08e65823b7a762b18
SubwayEnjoyer <epsilonexe2@gmail.com>
2023-08-23 20:25:57 -0400

    validaciones listas

--- src/app/Pages/inicio-sesion/inicio-sesion.page.ts
+++ src/app/Pages/inicio-sesion/inicio-sesion.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { validateRut } from '@fdograph/rut-utilities';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -7,12 +10,149 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioSesionPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router,private toastController: ToastController) { }
 
   ngOnInit() {
   }
+  // regex
   regexpass: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%#*?^&])[A-Za-z\d@$!#%*^?&]{8,50}$/;
-  sampleRegEx: RegExp = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]{2,100}$/;
+  regexname: RegExp = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]{2,100}$/;
+  regexCorreo: RegExp = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,4}$/;
+  regexTelefono: RegExp = new RegExp (/^[0-9]{8,8}$/);
+
+
+
+  // variables
+  nombre: string = '';
+  apellido: string = '';
+  rut: string = '';
+  contra : string = '';
+  contra2 : string = '';
+  correo: string = '';
+  telefono: string = '';
+  respuesta: string = '';
+  pregunta: string = '0';
+
+
+  // variables label
+  labelNombre: string = '';
+  labelApellido: string = '';
+  labelRut: string = '';
+  labelCorreo: string = '';
+  labelPregunta: string = '';
+  labelTelefono: string = '';
+  labelRespuesta: string = '';
+  labelContra: string = '';
+  labelContra2: string = '';
+ 
+
+
+
+  irPaginaPrincipal(){
+    // la bandera si está en verdadero permite que se envien los datos en caso contrario no lo permite
+    let bandera = true;
+
+    // validacion nombre
+    if(!this.regexname.test(this.nombre)){
+      bandera = false;
+      this.nombre = '';
+      this.labelNombre = 'Debe ingresar un nombre valido.';
+    }else{
+      this.labelNombre = '';
+    }
+
+    // validacion apellido
+    if(!this.regexname.test(this.apellido)){
+      bandera = false;
+      this.apellido = '';
+      this.labelApellido = 'Debe ingresar un apellido valido.';
+    }else{
+      this.labelApellido = '';
+    }
+
+    // validacion this.rut
+    if (!validateRut(this.rut)) {
+      bandera = false;
+      this.rut = '';
+      this.labelRut = 'Debe ingresar un rut valido.';
+    }else{
+      this.labelRut = '';
+    }
+
+    // validacion contraseña
+    if(!this.regexpass.test(this.contra)){
+      bandera = false;
+      this.contra = '';
+      this.labelContra = 'Debe ingresar una contraseña con carateres especiales, mayuscula y numero con minimo de 8 y maximo de 50';
+
+    }else{
+      this.labelContra = '';
+    }
+
+    // validacion igualdad de las dos contraseñas
+    if(this.contra != this.contra2){
+      bandera = false;
+      this.contra2 = '';
+      this.labelContra2 = 'Las contraseñas deben ser iguales';
+    }else{
+        this.labelContra2 = '';
+    }
+
+    // validacion correo
+    if (!this.regexCorreo.test(this.correo)) {
+      bandera = false;
+      this.correo = '';
+      this.labelCorreo = 'Debe ingresar un correo valido';
+    }else{
+      this.labelCorreo = '';
+    }
+    
+    // validacion pregunta
+    if(parseInt(this.pregunta)<1){
+      bandera = false;
+      this.labelPregunta = 'Debe ingresar una opcion';
+    }else{
+      this.labelPregunta = '';
+    }
+
+    // validacion respuesta
+    if(this.respuesta.length < 5 || this.respuesta.length > 200){
+      bandera = false;
+      this.respuesta = '';
+      this.labelRespuesta = 'Debe ingresar una respuesta de minimo 5 caracteres y maximo 200';
+    }else{
+      this.labelRespuesta = '';
+    }
+
+    // validacion telefono
+    if(!this.regexTelefono.test(this.telefono)){
+      bandera = false;
+      this.telefono = '';
+      this.labelTelefono = 'El telefono debe ser tener 8 numeros.';
+    }else{
+      this.labelTelefono = '';
+    }
+
+    if(bandera){
+      this.router.navigate(['/perfil']);
+      this.presentToast('bottom');
+    }
+    
+
+    
+
+  }
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'Cuenta creada exitosamente',
+      duration: 1500,
+      position: position,
+    });
+
+    await toast.present();
+  }
+
+  
   
 
 }
--- src/app/Pages/modificar-perfil/modificar-perfil.page.ts
+++ src/app/Pages/modificar-perfil/modificar-perfil.page.ts
@@ -10,17 +10,76 @@ import { ToastController } from '@ionic/angular';
 export class ModificarPerfilPage implements OnInit {
 
   constructor(private router:Router,private toastController: ToastController) { }
+  nombre: string = '';
+  apellido: string = '';
+  correo: string = '';
+  telefono: string = '';
+
+   // variables label
+   labelNombre: string = '';
+   labelApellido: string = '';
+   labelCorreo: string = '';
+   labelTelefono: string = '';
+
+   //regex
+  regexname: RegExp = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]{2,100}$/;
+  regexCorreo: RegExp = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,4}$/;
+  regexTelefono: RegExp = new RegExp (/^[0-9]{8,8}$/);
+
+   
 
   ngOnInit() {
   }
   irPaginaPrinicipal(){
-    this.router.navigate(['/pagina-principal']);
+
+
+
+    // la bandera si está en verdadero permite que se envien los datos en caso contrario no lo permite
+    let bandera = true;
+
+    // validacion nombre
+    if(!this.regexname.test(this.nombre)){
+      bandera = false;
+      this.nombre = '';
+      this.labelNombre = 'Debe ingresar un nombre valido.';
+    }else{
+      this.labelNombre = '';
+    }
+
+    // validacion apellido
+    if(!this.regexname.test(this.apellido)){
+      bandera = false;
+      this.apellido = '';
+      this.labelApellido = 'Debe ingresar un apellido valido.';
+    }else{
+      this.labelApellido = '';
+    }
+    // validacion correo
+    if (!this.regexCorreo.test(this.correo)) {
+      bandera = false;
+      this.correo = '';
+      this.labelCorreo = 'Debe ingresar un correo valido';
+    }else{
+      this.labelCorreo = '';
+    }
+    if(!this.regexTelefono.test(this.telefono)){
+      bandera = false;
+      this.telefono = '';
+      this.labelTelefono = 'El telefono debe ser tener 8 numeros.';
+    }else{
+      this.labelTelefono = '';
+    }
+
+    if(bandera){
+    this.router.navigate(['/perfil']);
     this.presentToast('bottom');
+    }
+
   }
 
   async presentToast(position: 'top' | 'middle' | 'bottom') {
     const toast = await this.toastController.create({
-      message: 'Bienvenido a Satiscar',
+      message: 'Datos cambiados exitosamente',
       duration: 1500,
       position: position,
     });
