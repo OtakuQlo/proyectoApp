573e651a88519336ddec11015971f4fcb1e366dc
OtakuQlo_ <manuelenrriquerivera@gmail.com>
2023-08-28 20:37:34 -0400

    Update

--- src/app/Pages/anadir-producto/anadir-producto.page.ts
+++ src/app/Pages/anadir-producto/anadir-producto.page.ts
@@ -22,33 +22,51 @@ export class AnadirProductoPage implements OnInit {
   kilometrajeV: number = 0; 
   transmisionV: string ="";
 
+  regexname: RegExp = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]{2,100}$/;
+
+  labelModelo: string = '';
+  labelPrecio: string = '';
+  labelAnos: string = '';
+  labelDescrp: string = '';
+  labelKilometraje: string = '';
+  labelSelects: string = '';
+
   pass: number = 0;
 
-  arrTrans : any[] = ["Automatica", "Semiautomatica", "Manual"];
+  arrTrans : string[] = ["Automatica", "Semiautomatica", "Manual"];
 
   ngOnInit() {
   }
 
   irPaginaPrincipal(){
     if(this.modeloV != ""  && this.descrpV != "" ){
-      if(this.modeloV.length > 2){
-        this.pass = 0;
+      if(this.regexname.test(this.modeloV)){
+        this.pass = 1;
+        this.labelModelo = 'El modelo debe contener como minimo 2 caracteres no especiales.';
       }
       if(this.descrpV.length < 10){
         this.pass = 1;
+        this.labelDescrp = 'La descripcion debe contener como minimo 10 caracteres.';
       }
     }else{
-      this.pass = 1;
+      this.pass = 0;
     }
+
     if (this.transmisionV != "" && this.colorV != "" && this.marcaV != "") {
       this.pass = 0;
+    }else{
+      this.labelSelects = 'Porfavor seleccione una opción.'
     }
+
     if( this.precioV != 0 && this.precioV > 0 && this.anosV != 0 && this.anosV > 0 && this.kilometrajeV != 0 && this.kilometrajeV > 0){
       this.pass = 0;
+
       if (this.kilometrajeV > 320000 ) {
         this.pass = 1;
+        this.labelKilometraje = 'El auto no puede ser publicado si su kilometraje pasa los 320,000.';
       }
     }
+
     if(this.pass == 0){
       this.router.navigate(['/pagina-principal']);
       this.presentToast('bottom');
