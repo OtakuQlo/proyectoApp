b8a791c118eccf3c58c83a8bd1ee309d4089d7c2
SubwayEnjoyer <107148747+SubwayEnjoyer@users.noreply.github.com>
2023-08-29 18:15:11 -0400

    cambios menores de revision del profe

--- src/app/Pages/modificar-perfil/modificar-perfil.module.ts
+++ src/app/Pages/modificar-perfil/modificar-perfil.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ModificarPerfilPageRoutingModule } from './modificar-perfil-routing.module';
 
 import { ModificarPerfilPage } from './modificar-perfil.page';
+import { ComponentsModule } from 'src/app/Components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ModificarPerfilPageRoutingModule
+    ModificarPerfilPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [ModificarPerfilPage]
 })
