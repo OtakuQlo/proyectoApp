f829d1b75efdc0d5aec48692b7e3d3a2c746d991
SubwayEnjoyer <epsilonexe2@gmail.com>
2023-09-13 21:28:20 -0300

    Codigo de base de datos listo

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,66 +1,77 @@
 import { Injectable } from '@angular/core';
 import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
+import { AlertController, Platform } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
+  //Variables
+  public db!:SQLiteObject;
 
-  db!:SQLiteObject;
-  
-  nombre:string = '';
-  apellido:string = '';
-  
+  //Tablas
+  persona: string = 'CREATE IF NOT EXISTS TABLE persona ();'
 
-  constructor(private sqlite: SQLite) { 
-    this.createDatabase();
+  //Datos tablas
+  datosPersona: string = 'INSERT OR IGNORE INTO persona values ()' 
+
+
+
+  //Observable de las tablas
+  observer = new BehaviorSubject([]);
+
+  //Observable de la base de datos
+  private isDBReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  constructor(private sqlite: SQLite, private platform: Platform, private alertController: AlertController) { 
+
   }
 
-  createDatabase() {
+
+
+  crearDB(){
+    this.platform.ready().then(() =>
     this.sqlite.create({
       name: 'satiscar.db',
       location: 'default'
-    })
-    .then(() => {alert('base de datos creada')
-      
-    })
-    .catch(e => console.log(e));
+    }).then((db:SQLiteObject) =>{
+      this.db = db
+      this.crearTablas()
+
+    }).catch((e) => {
+      this.presentAlert('error en la crearDB '+ e);
+    }));
   }
 
-  createTable(){
-    try{
-    this.db.sqlBatch([
-    ['CREATE TABLE IF NOT EXISTS  persona (nombre VARCHAR(50) NOT NULL, apellido VARCHAR(50)) NOT NULL'],
-    ]).then(() => alert('tablas creadas'))
-    .catch((e) => alert(JSON.stringify(e)))
-  }catch(error:any){
-    alert(error);
-  }
-  }
 
-  insertData(){
+
+  async crearTablas(){
     try{
-      let query = 'insert into persona values(miguel, perez)';
-      this.db.executeSql(query,[])
-    .then(() => alert('datos ingresados a la tabla'))
-    .catch((e) => alert(JSON.stringify(e)))
-    }catch(error:any){
-      alert(error);
+      await this.db.executeSql(this.persona,[]);
+      await this.db.executeSql(this.datosPersona,[])
+      this.isDBReady.next(true);
+
+    }catch(error: any){
+      this.presentAlert("Error en crear las tablas" + error)
     }
+
+
+
+
   }
 
-  showData(){
-    try{
-      this.db.executeSql('SELECT * FROM persona',[])
-      .then((result)=> {
-        for (let i = 0; i < result.rows.lenght ; i++) {
-          this.nombre = result.rows.item(1).nombre,
-          this.apellido = result.rows.item(1).apellido
-        }
-      })
-      .catch((e) => JSON.stringify(e))
-    }catch(error : any){
-      alert(error);
-    }
+  async presentAlert(msj:string) {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: msj,
+      buttons: ['OK'],
+    });
+
+
+    await alert.present();
   }
+
+  
 }
